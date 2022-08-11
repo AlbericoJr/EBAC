0b7d3abcd86d355efdbd03aef9dde4fa0596c9f4
Albérico Junior <40373171+AlbericoJr@users.noreply.github.com>
2022-08-11 20:50:56 -0300

    Calculo da média

---
+++ Calculo_media/script.js
@@ -0,0 +1,11 @@
+function calcular(){
+    var n1 = document.getElementById("n1").value;
+    var n2 = document.getElementById("n2").value;
+    var n3 = document.getElementById("n3").value;
+
+    var media = (Number(n1) + Number(n2) + Number(n3)) / 3;
+
+    alert(` A média dos valores é: ${media}`);
+
+    console.log(` A média dos valores é: ${media}`);
+}
